1bf8811568eb984c3af0943f7f558fbcb426f8cd
anaghayawale <anaghayawale7@gmail.com>
2023-11-23 21:36:11 +0530

    added signup logic

--- lib/ui/screens/sign_up_screen.dart
+++ lib/ui/screens/sign_up_screen.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:tasks/ui/screens/login_screen.dart';
 import 'package:tasks/ui/widgets/custom_button.dart';
 import 'package:tasks/ui/widgets/custom_heading.dart';
 import 'package:tasks/ui/widgets/custom_textfield.dart';
-
+import 'package:http/http.dart' as http;
 import '../widgets/custom_snackbar.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -18,8 +20,13 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _repassController = TextEditingController();
+  bool isLoading = false;
 
-  void _registerUser() {
+  void _registerUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
     String name = _nameController.text.trim();
     String email = _emailController.text.trim();
     String password = _passwordController.text.trim();
@@ -30,6 +37,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         snackbarText: 'Please enter all the fields',
         snackbarTextColor: Colors.red,
       ).showSnackBar(context);
+      return;
     }
 
     if (!RegExp(r'[a-zA-Z]').hasMatch(name)) {
@@ -37,6 +45,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         snackbarText: 'Name should contain letters',
         snackbarTextColor: Colors.red,
       ).showSnackBar(context);
+      return;
     }
 
     if (!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
@@ -45,14 +54,90 @@ class _SignUpScreenState extends State<SignUpScreen> {
         snackbarText: 'Please enter a valid email address',
         snackbarTextColor: Colors.red,
       ).showSnackBar(context);
+      return;
     }
 
     if (password != repass) {
       const CustomSnackbar(
-              snackbarText: 'Passwords do not match',
-              snackbarTextColor: Colors.red)
-          .showSnackBar(context);
+        snackbarText: 'Passwords do not match',
+        snackbarTextColor: Colors.red,
+      ).showSnackBar(context);
+      return;
     }
+
+    
+
+    BuildContext currentContext = context;
+
+    // Call the API to register the user
+    try {
+      var response = await http.post(
+          Uri.parse('http://taskmasterapp.vercel.app/api/sendmail'),
+          body: jsonEncode({
+            "name": name,
+            "email": email,
+            "password": password,
+          }));
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 308 &&
+          response.headers.containsKey('location')) {
+        var redirectUrl = response.headers['location'];
+        // print('Redirect URL: $redirectUrl');
+
+        // Make another request to the redirect URL
+        var redirectResponse = await http.post(Uri.parse(redirectUrl!),
+            body: jsonEncode({
+              "name": name,
+              "email": email,
+              "password": password,
+            }));
+
+        // Process the response from the redirect
+        print('Redirect Response status: ${redirectResponse.statusCode}');
+        print('Redirect Response body: ${redirectResponse.body}');
+
+        // Parse the response body as JSON
+        var responseBody = jsonDecode(redirectResponse.body);
+
+        // Get the message from the JSON response
+        var message = responseBody['message'];
+        var success = responseBody['success'];
+        
+        // Show the message in a Snackbar
+        if (success == true) {
+          // ignore: use_build_context_synchronously
+          showVerificationSentMessage(currentContext, message, Colors.green);
+        } else if (success == false){
+          var error = responseBody['error'];
+          // ignore: use_build_context_synchronously
+          showVerificationSentMessage(currentContext, error, Colors.blue);
+        }
+
+        setState(() {
+          isLoading = false;
+        });
+      }
+
+    
+    } catch (e) {
+      
+      showVerificationSentMessage(
+          currentContext, 'Try again - An error occurred', Colors.red);
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  void showVerificationSentMessage(
+      BuildContext context, String message, Color snackbarColor) {
+    CustomSnackbar(
+      snackbarText: message,
+      snackbarTextColor: snackbarColor,
+    ).showSnackBar(context);
   }
 
   @override
@@ -106,7 +191,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     height: 20,
                   ),
                   CustomButton(
-                      buttonText: 'Create account', onPressed: _registerUser),
+                    buttonText: 'Create account',
+                    onPressed: _registerUser,
+                    
+                  ),
                   const SizedBox(
                     height: 15,
                   ),
